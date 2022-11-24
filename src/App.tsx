d09beedb7d4a1339dca9fe6923db7eed279c80da
AbAyvazyan333 <108995486+AbAyvazyan333@users.noreply.github.com>
2022-11-24 17:43:49 +0400

    ready menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -106,26 +106,95 @@ function App() {
                </div>
             </div>
 
-          <div className={'notification-part'}>
-                <div className={'notification-section'}>
-                    Notifications
+          {/*notification part-----------------------------------------------------------------------------------------*/}
+
+          {/*<div className={'notification-part'}>*/}
+          {/*      <div className={'notification-section'}>*/}
+          {/*          Notifications*/}
+          {/*      </div>*/}
+          {/*    <div className={'section-of-notifications'}>*/}
+          {/*      <div className={'single-notification'}>*/}
+          {/*          <img src={userIcon} alt="User"/>*/}
+          {/*          <span style={{fontWeight:'bold'}}>UserName</span>*/}
+          {/*          <span>started following you</span>*/}
+          {/*          <button className={'notify-button-following'}>Following</button>*/}
+          {/*      </div>*/}
+          {/*        <div className={'single-notification'}>*/}
+          {/*            <img src={userIcon} alt="User"/>*/}
+          {/*            <span style={{fontWeight:'bold'}}>UserName</span>*/}
+          {/*            <span>started following you</span>*/}
+          {/*            <button className={'notify-button-following'}>Following</button>*/}
+          {/*        </div>*/}
+          {/*        <div className={'single-notification'}>*/}
+          {/*            <img src={userIcon} alt="User"/>*/}
+          {/*            <span style={{fontWeight:'bold'}}>UserName</span>*/}
+          {/*            <span>liked your post</span>*/}
+          {/*            <button className={'notify-button-toFollow'}>Follow</button>*/}
+
+          {/*        </div>*/}
+          {/*    </div>*/}
+          {/*</div>*/}
+
+          {/*----------------------------------------------------------------------------------------------------*/}
+
+
+
+          <div className={'search-part'}>
+              <div className={'search-upper-part'}>
+                <div className={'search-section'}>Search</div>
+                <div className={'search-input-part'}>
+                    <input type="text" placeholder={'Search'}/>
                 </div>
-              <div className={'section-of-notifications'}>
-                <div className={'single-notification'}>
-                    <img src={userIcon} alt="User"/>
-                    <span style={{fontWeight:'bold'}}>UserName</span>
-                    <span>started following you</span>
-                </div>
-                  <div className={'single-notification'}>
-                      <img src={userIcon} alt="User"/>
-                      <span style={{fontWeight:'bold'}}>UserName</span>
-                      <span>started following you</span>
-                  </div>
-                  <div className={'single-notification'}>
-                      <img src={userIcon} alt="User"/>
-                      <span style={{fontWeight:'bold'}}>UserName</span>
-                      <span>started following you</span>
-                  </div>
+              </div>
+
+              <div className={'search-main-part'}>
+                    <div className={'search-instruction-part'}>
+                        <span style={{fontSize:'14px'}}>Recent</span>
+                        <span className={'clear-searches-button'}>Clear all</span>
+                    </div>
+
+                    <div className={'search-singleUser-part'}>
+                        <div className={'single-search-user'}>
+                            <div className={'single-search-icon-part'}>
+                                <img src={userIcon} alt="userIcon"/>
+                            </div>
+                            <div className={'single-search-text-part'}>
+                                <div>username</div>
+                                <div>bio about username</div>
+                            </div>
+                            <div className={'single-search-delete-part'}>
+                                <span>&#10005;</span>
+                            </div>
+                        </div>
+
+                        <div className={'single-search-user'}>
+                            <div className={'single-search-icon-part'}>
+                                <img src={userIcon} alt="userIcon"/>
+                            </div>
+                            <div className={'single-search-text-part'}>
+                                <div>username</div>
+                                <div>bio about username</div>
+                            </div>
+                            <div className={'single-search-delete-part'}>
+                                <span>&#10005;</span>
+                            </div>
+                        </div>
+
+                        <div className={'single-search-user'}>
+                            <div className={'single-search-icon-part'}>
+                                <img src={userIcon} alt="userIcon"/>
+                            </div>
+                            <div className={'single-search-text-part'}>
+                                <div>username</div>
+                                <div>bio about username</div>
+                            </div>
+                            <div className={'single-search-delete-part'}>
+                                <span>&#10005;</span>
+                            </div>
+                        </div>
+                    </div>
+
+
               </div>
           </div>
       </>
